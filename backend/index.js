57f915abfde4c1f32a32d42fb37e05b2052f6e65
Pranav Verma <72593204+Pranav-1001@users.noreply.github.com>
2023-03-25 12:02:55 +0530

    Added hashing, salt and JWT support

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
-const connectToMongo = require('./db');
+const connectToMongo = require("./db");
 connectToMongo();
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+// Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
